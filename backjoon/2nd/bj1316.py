92f1617cbb321140966aaf11df6fe3fe656e972d
Shihyun Han <devowl99@gmail.com>
2024-04-14 10:16:33 +0900

    enter

--- backjoon/2nd/bj1316.py
+++ backjoon/2nd/bj1316.py
@@ -1,32 +1,24 @@
 # 왜 안될까
-
-import sys
-input = sys.stdin.readline
-
 n = int(input())
 word = []
 gw = 0
 
 for _ in range(n):
-    word.append(input().strip())
+    word.append(input())
 
 for i in word:
     ilen = len(i)
     is_gw = True
 
-    if ilen <= 2 or len(set(i)) == 1:
-        gw += 1
-        # print('case1 : ' + i)
-    else:
+    if ilen > 2 and len(set(i)) != 1:
         for j in range(ilen-2):
             for k in range(j+2, ilen):
                 if i[j] == i[k]:
                     is_gw = False
-                    break
-            
-        if is_gw:
-            gw += 1
-            # print('case2 : ' + i)
-        ngw = 0
+                    break    
+            if not is_gw:
+                break
+    if is_gw:
+        gw += 1
 
 print(gw)
