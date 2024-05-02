e63b7ae88ad9942abe066565ffaeb3ab607aed09
Shihyun Han <devowl99@gmail.com>
2024-05-02 23:55:18 +0900

    enter

--- backjoon/2nd/자료구조/10845.py
+++ backjoon/2nd/자료구조/10845.py
@@ -1,1 +1,32 @@
-#미완
+import sys
+input = sys.stdin.readline
+n = int(input())
+q = []
+
+for _ in range(n):
+    com = input().split()
+
+    if com[0] == 'pop':
+        if (len(q)!=0):
+            print(q.pop(0))
+        else:
+            print(-1)
+    elif com[0] == 'size':
+        print(len(q))
+    elif com[0] == 'empty':
+        if (len(q)==0):
+            print(1)
+        else:
+            print(0)
+    elif com[0] == 'front':
+        if (len(q)!=0):
+            print(q[0])
+        else:
+            print(-1)
+    elif com[0] == 'back':
+        if (len(q)!=0):
+            print(q[-1])
+        else:
+            print(-1)
+    else:
+        q.append(com[1])
