4c638c594283f5193302194a7ca9da0eec59a291
Shihyun Han <devowl99@gmail.com>
2024-05-16 19:00:23 +0900

    enter

--- backjoon/3rd/1978.py
+++ backjoon/3rd/1978.py
@@ -1,3 +1,14 @@
 n = int(input())
-num = list(map(int, input().split))
+num = list(map(int, input().split()))
+count = 0
 
+for i in num:
+    pn = True
+    for j in range(2,i):
+        if i % j  == 0:
+            pn = False
+            break
+    if i != 1 and pn:
+        count += 1
+    
+print(count)
